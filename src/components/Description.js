39428259fd29cfa3f8cbfbe751341873d716af5f
emmanuel <emmanueliyare7@gmail.com>
2021-11-14 13:06:51 +0100

    back button in description  updated

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 function Description(props) {
+
+  let history = useHistory()
+
   return (
     <div>
       <h1>Description</h1>
+      <button onClick={() => {history.push("/")}}>Back</button>
     </div>
   )
 }
